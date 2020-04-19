07b382a340b70112dec799f582f7adbe876054d4
Steven Kampen <steven@peertjelabs.nl>
2020-04-19 18:36:12 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import { App } from './app';
+import { name as appName } from './app-meta.json';
+
+AppRegistry.registerComponent(appName, () => App);
